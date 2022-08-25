ed61d7ade0859691f95d9ac06e88ed41834f2251
Mihail <mcoscodan@ranorex.com>
2022-08-25 20:56:29 +0300

    Sell Product use case be part

---
+++ Supermarket/UseCases/ProductsUseCases/SellProductUseCase.cs
@@ -0,0 +1,26 @@
+﻿using UseCases.DataStorePluginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases.ProductsUseCases
+{
+    public class SellProductUseCase : ISellProductUseCase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public SellProductUseCase(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public void Execute(int productId, int quantityToSell)
+        {
+            var productToSell = _productRepository.GetProductById(productId);
+            if (productToSell == null) return;
+            // change product quantity
+            productToSell.Quantity -= quantityToSell;
+
+            // update product
+            _productRepository.Update(productToSell);
+        }
+    }
+}
