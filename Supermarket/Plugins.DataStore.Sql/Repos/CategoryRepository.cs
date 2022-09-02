68ddb608f029b785b76f589200d956e1b7a0e9cc
Mihail <mcoscodan@ranorex.com>
2022-09-02 19:22:31 +0300

    Add Category repository

---
+++ Supermarket/Plugins.DataStore.Sql/Repos/CategoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.Sql.Repos
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly SuperMarketDbContext _dbContext;
+
+        public CategoryRepository(SuperMarketDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void AddCategory(Category category)
+        {
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteCategory(int categoryId)
+        {
+            var category = _dbContext.Categories.Find(categoryId);
+            if (category == null)
+            {
+                return;
+            }
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _dbContext.Categories.ToList();
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return _dbContext.Categories.Find(categoryId);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var categoryToUpdate = GetCategoryById(category.CategoryId);
+            categoryToUpdate.Name = category.Name;
+            categoryToUpdate.Description = category.Description;
+            _dbContext.SaveChanges();
+        }
+    }
+}
